666a6ffef406a3b66e0662c4dbf236edfe30dba1
Osman Işık <35349447+kisinamso@users.noreply.github.com>
2024-02-12 14:23:14 +0300

    Update and rename AllDatabaseSizeInInstance.sql to 2-AllDatabaseSizeInInstance.sql

--- 2-AllDatabaseSizeInInstance.sql
+++ 2-AllDatabaseSizeInInstance.sql
@@ -1,8 +1,8 @@
 SELECT
 	 SERVERPROPERTY('MachineName') AS ServerName
 	,d.NAME AS DatabaseName
-    ,ROUND(SUM(CAST(mf.size AS bigint)) * 8 / 1024, 0) Size_MBs
-    ,(SUM(CAST(mf.size AS bigint)) * 8 / 1024) / 1024 AS Size_GBs
+    	,ROUND(SUM(CAST(mf.size AS bigint)) * 8 / 1024, 0) Size_MBs
+    	,(SUM(CAST(mf.size AS bigint)) * 8 / 1024) / 1024 AS Size_GBs
 FROM sys.master_files mf
 INNER JOIN sys.databases d ON d.database_id = mf.database_id
 WHERE d.database_id > 4 -- Skip system databases if you want
