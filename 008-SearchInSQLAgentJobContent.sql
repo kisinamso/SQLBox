7b33ca42fde027c11f67be6268a7a3b04e577407
Osman Işık <35349447+kisinamso@users.noreply.github.com>
2024-02-12 15:15:04 +0300

    Update 008-SearchInSQLAgentJobContent.sql

--- 008-SearchInSQLAgentJobContent.sql
+++ 008-SearchInSQLAgentJobContent.sql
@@ -1,3 +1,9 @@
+/*
+-----------------------------------------------@kisinamso-----------------------------------------------
+|This script works for searching key in SQL Server Agent Jobs.                                         |
+-----------------------------------------------@kisinamso-----------------------------------------------
+*/
+
 USE [msdb]
 GO
 
@@ -6,9 +12,8 @@ SELECT
     ,[sJOB].[name]		AS [JobName]
     ,[step].step_name
     ,[step].command
-FROM
-    [msdb].[dbo].[sysjobs] AS [sJOB]
-    LEFT JOIN [msdb].dbo.sysjobsteps step ON sJOB.job_id = step.job_id
+FROM [msdb].[dbo].[sysjobs] AS [sJOB]
+LEFT JOIN [msdb].dbo.sysjobsteps step ON sJOB.job_id = step.job_id
 
 WHERE [step].[command] LIKE '%ENTERSEARCHKEYWORD%'
 ORDER BY [JobName]
